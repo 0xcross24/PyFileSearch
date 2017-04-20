f9d45c78afa81acfce55b12e2ad9c3747e4dd9bf
Cross113 <dmoon1112@gmail.com>
2017-04-20 02:26:31 -0400

    First commit

---
+++ filesearch.py
@@ -0,0 +1,33 @@
+import os;
+import time;
+
+directory = 'C:\Users\Danny';
+search_file = raw_input("\nEnter the file name: ");
+
+def traverse(search_file):
+	count = 0;
+	found = False;
+	print "\n";
+	for root, dirs, filenames in os.walk(directory):
+		
+		count += len(dirs);
+		print "\r[*] Searching through " + str(count) + " Directories",
+		time.sleep(0.00000001);
+
+		for file in filenames:
+			if file.endswith(search_file):
+				print "\n\n[*] File has been found! [*]\n";
+				print "\t[*] File Name: '%s' [*]\n" % file;
+				print "\t[*] Located: " + os.path.join(root, file) + "\n";
+				found = True;
+
+	if found == False:
+		print "\n[*] The File does not exist [*]\n";
+
+	print "\n";
+
+	
+
+traverse(search_file);
+
+
